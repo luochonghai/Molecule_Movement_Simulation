d5c04585d63941c2daf74e288ed86ada623fa657
GeorgeRobots <1258501438@qq.com>
2018-04-17 19:59:57 +0800

    Update read_cordinate_text.py

--- Assessment/read_cordinate_text.py
+++ Assessment/read_cordinate_text.py
@@ -1,6 +1,9 @@
 import random_index_cal
 import ast
 
+SIZE = 100
+TERMINAL = 1000
+
 def read_coordinate_from_text(file_path,height,width):
 	Cx = [[0 for i in range(width+1)] for j in range(height)]
 	Cy = [[0 for i in range(width+1)] for j in range(height)]
@@ -13,17 +16,17 @@ def read_coordinate_from_text(file_path,height,width):
 				break
 				pass
 			index,cx,cy,cz = lines.split()
-			Cx[int(index)][int(counter/100)] = ast.literal_eval(cx)
-			Cy[int(index)][int(counter/100)] = ast.literal_eval(cy)
-			Cz[int(index)][int(counter/100)] = ast.literal_eval(cz)
+			Cx[int(ast.literal_eval(index))][int(counter/SIZE)] = ast.literal_eval(cx)
+			Cy[int(ast.literal_eval(index))][int(counter/SIZE)] = ast.literal_eval(cy)
+			Cz[int(ast.literal_eval(index))][int(counter/SIZE)] = ast.literal_eval(cz)
 			counter += 1	
 
 	file_to_read.close()
 	return Cx,Cy,Cz
 
 def model_evaluation():
-	Cx,Cy,Cz = read_coordinate_from_text('D:\\FDU\\Template\\CS\\数学建模\\coordinate.txt',100,100)
-	data = random_index_cal.Data_manipulate(100,0.1,1,1,60,100)
+	Cx,Cy,Cz = read_coordinate_from_text('D:\\FDU\\Template\\CS\\数学建模\\coordinate.txt',SIZE,TERMINAL)
+	data = random_index_cal.Data_manipulate(SIZE,0.1,1,1,60,TERMINAL)
 	Vx = data.v_cal_list(Cx)
 	Vy = data.v_cal_list(Cy)
 	Vz = data.v_cal_list(Cz)
