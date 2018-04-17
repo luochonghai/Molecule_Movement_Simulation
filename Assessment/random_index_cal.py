b3ffcae1cc3485353cb0137801f3c5a9b77fb27c
GeorgeRobots <1258501438@qq.com>
2018-04-17 18:25:01 +0800

    Update random_index_cal.py

--- Assessment/random_index_cal.py
+++ Assessment/random_index_cal.py
@@ -44,6 +44,7 @@ class Data_manipulate:
         row_3 = booksheet.row_values(0)
         return booksheet
 
+    #sheetdata version
     def v_cal(self,sheetdata):
         sum_v = [[0 for i in range(self.terminal-1)]]
 
@@ -59,15 +60,31 @@ class Data_manipulate:
 
         return sum_v
 
+    #list version
+    def v_cal_list(self,sheetdata):
+        sum_v = [[0 for i in range(self.terminal-1)]]
+
+        for i in range(self.sample_size):
+            temp_v = [[0 for k in range(self.terminal-1)]]
+            for j in range(self.terminal-1):
+                temp_v[0][j] = sheetdata[i][j+1]-sheetdata[i][j]
+                temp_v[0][j] /= self.interval
+            if(i == 0):
+                sum_v = temp_v
+            else:
+                sum_v += temp_v    
+
+        return sum_v
+
     def variable_v_len(self,Vx,Vy,Vz):
         v_len_matrix = [[0 for i in range(self.terminal-1)] for j in range(self.sample_size)]
-        v_square = v_len_matrix
+        v_square = [[0 for i in range(self.terminal-1)] for j in range(self.sample_size)]
         result = [0 for i in range(self.terminal-1)]
-        sum_square = result
-        vector_sum = result
-        vector_sum_x = result
-        vector_sum_y = result
-        vector_sum_z = result
+        sum_square = [0 for i in range(self.terminal-1)]
+        vector_sum = [0 for i in range(self.terminal-1)]
+        vector_sum_x = [0 for i in range(self.terminal-1)]
+        vector_sum_y = [0 for i in range(self.terminal-1)]
+        vector_sum_z = [0 for i in range(self.terminal-1)]
         for i in range(self.sample_size):
             for j in range(self.terminal-1):
                 v_square[i][j] = Vx[i][j]*Vx[i][j]+Vy[i][j]*Vy[i][j]+Vz[i][j]*Vz[i][j]
@@ -82,8 +99,8 @@ class Data_manipulate:
         self.v_direction_detect(v_len_matrix,Vx,'x')
         self.v_direction_detect(v_len_matrix,Vy,'y')
         self.v_direction_detect(v_len_matrix,Vz,'z')
-        #self.drawCumulativeHist(v_len_matrix)
-        #self.drawHist(v_len_matrix)
+        self.drawCumulativeHist(v_len_matrix)
+        self.drawHist(v_len_matrix)
 
         #calculate the mean of velocity
         for i in range(self.terminal-1):
@@ -122,6 +139,20 @@ class Data_manipulate:
         pyplot.title('_frequency_statistic_plot of v_direction_'+direct)
         pyplot.show()        
 
+def cpp_invoke():
+    data = Data_manipulate(246,0.01,1,0.2,19,100)
+    Sx = data.data_input('D:\\FDU\\Template\\CS\\数学建模\\S.xlsx','Sx')
+    Vx = data.v_cal(Sx)
+    Sy = data.data_input('D:\\FDU\\Template\\CS\\数学建模\\S.xlsx','Sy')
+    Vy = data.v_cal(Sy)
+    Sz = data.data_input('D:\\FDU\\Template\\CS\\数学建模\\S.xlsx','Sz')
+    Vz = data.v_cal(Sz)   
+    #variable of length_velocity 
+    temp = data.variable_v_len(Vx,Vy,Vz)
+    variable_v = temp[0]
+    mean_v = temp[1]
+    sum_vec = temp[2]
+    result = data.random_index(variable_v,mean_v,sum_vec)
 
 if __name__ == '__main__':
     data = Data_manipulate(246,0.01,1,0.2,19,100)
