6e590e795d9596e77784eeef0ef1c9064e1596ac
GeorgeRobots <1258501438@qq.com>
2018-06-11 00:27:38 +0800

    Add files via upload

---
+++ cpp_version/calculation_tool.h
@@ -0,0 +1,25 @@
+#include"stdafx.h"
+using namespace std;
+
+//two-vector multiplication
+double v2_multi(double*a, double*b, int len)
+{
+	double res = 0.0;
+	for (int i = 0; i < len; ++i)
+		res += a[i] * b[i];
+	return res;
+}
+
+//calculate vector-2-cos
+double v2_cos(double *a,double*b, int len)
+{
+	return v2_multi(a, b, len) / (sqrt(v2_multi(a,a,len)*v2_multi(b,b,len)));
+}
+
+//normalize the vector,to store the result in res
+void norm_vec(double*a, double* res,int len)
+{
+	double v_len = sqrt(v2_multi(a,a,len));
+	for (int i = 0; i < len; ++i)
+		res[i] = a[i]/v_len;
+}
